9bd65c60ce9bc31aa114a156faf0aa69b02fc380
babucke <babucke.paul@gmail.com>
2014-04-14 23:10:15 +0200

    *Healthbar(For Mark) not ready

---
+++ Unity/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healthbar : MonoBehaviour {
+	
+	GUIStyle style = new GUIStyle();
+	Texture2D texture;
+	Color redColor = Color.red;
+	Color greenColor = Color.green;
+	//GameObject buddy = GameObject.Find ("Player");
+	//PlayerHealth p = buddy.GetComponent<PlayerHealth> ();
+	PlayerHealth p = new PlayerHealth();
+
+	
+	void Start()
+	{
+		//GameObject Player = GameObject.Find ("Player");
+		//PlayerHealth p = Player.GetComponent <PlayerHealth> ();
+
+		texture = new Texture2D(1, 1);
+		texture.SetPixel(1, 1, greenColor);
+	}
+	
+	private void Update()
+	{
+		
+		if (p.health > 50)
+		{
+			texture.SetPixel(1, 1, greenColor);
+		}
+
+		if (p.health < 50)
+		{
+			texture.SetPixel(1, 1, redColor);
+		}
+
+	}
+	
+	public void OnGUI()
+	{	
+		
+		texture.Apply();
+		
+		style.normal.background = texture;
+		GUI.Box(new Rect(0, 0, (p.health*3), 50), new GUIContent(""), style);
+	}
+}
--- Unity/Assets/Scripts/PlayerSkills.cs
+++ Unity/Assets/Scripts/PlayerSkills.cs
@@ -14,6 +14,7 @@ public class PlayerSkills : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Interaction ();
+
 	}
 
 	void Interaction()
