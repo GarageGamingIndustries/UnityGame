41688a742ceb45bd866c4073df059957da35633c
reavel <sroehl@gmx.net>
2014-04-14 17:04:23 +0200

    * added PlayerHealth script

---
+++ Unity/Assets/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float health = 100f;
+	public AudioClip deathClip;
+	private bool playerDead;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (health <= 0f) {
+
+		}
+	}
+}
