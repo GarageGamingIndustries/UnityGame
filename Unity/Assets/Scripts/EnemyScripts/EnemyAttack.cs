e3533376c6e4c168ff424f32ee43ed02663ff41d
Mark W <mark1203@gmx.de>
2014-04-17 11:53:06 +0200

    *HP-Bar modified *Enemy has now random Crits *Hp should be -1, when dead - but doesn't work yet (PlayerHealth.cs:28)

--- Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class EnemyAttack : MonoBehaviour {
 	
 	public GameObject target;
 	public float attackTimer;
 	public float coolDown;
+	public float EnemyDamage;
+	public float CritChance;
+	public float CritDmg;
 	
 	// Use this for initialization
 	void Start () {
 		attackTimer = 0;
-		coolDown = 2.0f;
+		//Original cooldown = 2.0f
+		coolDown = 0.5f;
+		EnemyDamage = 10;
+		CritChance = 10f; // 10% - should be calculated later based on level difference
+		CritDmg =1.2f; //120% Dmg;
 	}
 	
 	// Update is called once per frame
@@ -35,9 +43,17 @@ public class EnemyAttack : MonoBehaviour {
 		
 		if(distance < 1.7){
 			if(direction > 0){
-				PlayerHealth.actualHealth -= 10;
+				//Attacks only when alive - Stops if dead
+				if(PlayerHealth.actualHealth >0f){
+					float crit = Random.Range (0,100);
+					if(crit > CritChance){
+						PlayerHealth.actualHealth -= EnemyDamage;
+					}
+					else {
+						PlayerHealth.actualHealth -= EnemyDamage * CritDmg;
+					}
+				}
 			}
 		}
-		
 	}
 }
